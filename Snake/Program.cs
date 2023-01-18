f9d49c7875b2a554e89ba23a3bfb7dbb5a2513b9
Muhammed Emir Köroglu <muhammedem.k@gso.schule.koeln>
2023-01-18 12:12:22 +0100

    - Man kann sich jetzt die Statistiken anzeigen lassen
    - Es gibt jetzt eine Funktion, womit man sehen kann, wie lange man überlebt hat
    
    - weitere Kommentare wurden hinzugefügt

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -4,11 +4,13 @@ using static System.Formats.Asn1.AsnWriter;
 using System.Xml.Linq;
 using System.Speech.Synthesis;
 using System.Threading.Channels;
+using System.Diagnostics;
 
 class Snake
 {
     static void Main(string[] args)
     {
+        Stopwatch time = new Stopwatch();
         int x = 10, y = 10; // Startposition des Schlangenkopfs
         int foodX = 15, foodY = 15; // Startposition des Futters
         int score = 0;
@@ -31,7 +33,8 @@ class Snake
 
         if (menu == 1)
         {
-            Console.WriteLine("Cooming soon...\n");
+            Console.WriteLine("Score: " + score);
+            Console.WriteLine("Überlebende Zeit: {0}\n", time.Elapsed);
             Thread.Sleep(1000);
 
             Console.WriteLine("Bitte drücken Sie ENTER um ins Menü zu gelangen!");
@@ -41,6 +44,7 @@ class Snake
         }
         if (menu == 2)
         {
+            time.Start();
         while (true)
         {
             Console.Clear(); // Löscht den Bildschirm
@@ -49,7 +53,7 @@ class Snake
             Console.SetCursorPosition(foodX, foodY); // Setzt die Cursorposition
             Console.Write("X"); // Zeichnet das Futter
             Console.SetCursorPosition(0, 0); // Setzt die Cursorposition
-            Console.Write("Score: " + score); // Zeigt die Punktzahl an
+            Console.Write("Score:{0}        Time:{1}",score,time.Elapsed); // Zeigt die Punktzahl und wie lange man überlebt hat an
 
 
             input = Console.ReadKey(true).KeyChar; // Liest die Tasteneingabe
@@ -58,30 +62,34 @@ class Snake
             else if (input == 's') y++; // runter
             else if (input == 'a') x--; // links
             else if (input == 'd') x++; // rechts
-            if (x <= 0 || x >= Console.WindowWidth || y <= 0 || y >= Console.WindowHeight || snake.Contains(new Tuple<int, int>(x, y)))
+            if (x <= 0 || x >= Console.WindowWidth || y <= 0 || y >= Console.WindowHeight || snake.Contains(new Tuple<int, int>(x, y))) // Man kann nicht über die Borderline gehen mit der Schlange
             {
                 Console.Clear();
                 Console.Beep(330, 500);
                 synth.Speak("Game over");
                 Console.WriteLine("Game Over! Dein score ist: " + score);
-                Console.WriteLine("Drücke R um neuzustarten\n Q um das Spiel zu beenden\n M um wieder ins Menü zu gelangen");
+                Console.WriteLine("Drücke R um neuzustarten\n Drücke Q um das Spiel zu beenden\n Drücke M um wieder ins Menü zu gelangen");
                 input = Console.ReadKey(true).KeyChar;
-                if (input == 'r')
+                if (input == 'r') // Startet das Spiel neu
                 {
-                    x = 10;
-                    y = 10;
-                    foodX = 15;
-                    foodY = 15;
-                    score = 0;
-                    snake.Clear();
-                    snake.Add(new Tuple<int, int>(x, y));
+                        time.Stop();
+                        time.Reset();
+                        x = 10;
+                        y = 10;
+                        foodX = 15;
+                        foodY = 15;
+                        score = 0;
+                        snake.Clear();
+                        snake.Add(new Tuple<int, int>(x, y));
                 }
-                else if (input == 'q')
-                {
-                    break;
-                }
-                else if(input == 'm')
+                else if (input == 'q') // Beendet das Programm
                     {
+                        time.Stop();
+                        break;
+                    }
+                else if(input == 'm') // Bringt zurück ins Menü
+                    {
+                        time.Stop();
                         goto Anfang;
                     }
             }
@@ -91,7 +99,7 @@ class Snake
             {
                 foodX = new Random().Next(0, Console.WindowWidth); // Generiert eine neue x-Position für das Futter
                 foodY = new Random().Next(1, Console.WindowHeight);// Generiert eine neue y-Position für das Futter
-                    score++;
+                score++;
             }
 
             Thread.Sleep(100); // Wartet 100 Millisekunden
